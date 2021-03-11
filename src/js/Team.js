0635e2784078be074c031c85f3fd2d3cc505dc2a
theart84 <artshadow74@gmail.com>
2021-03-11 23:38:46 +0500

    fixed bugs

--- src/js/Team.js
+++ src/js/Team.js
@@ -1,43 +1,46 @@
 export default class Team {
   constructor() {
-    this.swordsman = {
-      name: 'Сэр_Ланцелот',
-      type: 'Swordsman',
-      health: 70,
-      level: 1,
-      attack: 40,
-      defence: 10,
-    };
+    this.teams = [
+      {
+        name: 'Сэр_Ланцелот',
+        type: 'Swordsman',
+        health: 70,
+        level: 1,
+        attack: 40,
+        defence: 10,
+      },
 
-    this.bowman = {
-      name: 'Логоваз',
-      type: 'Bowman',
-      health: 50,
-      level: 1,
-      attack: 40,
-      defence: 10,
-    };
-    this.magician = {
-      name: 'Гендальф',
-      type: 'Magician',
-      health: 60,
-      level: 3,
-      attack: 40,
-      defence: 10,
-    };
-    this.daemon = {
-      name: 'xxxTrueNagibXxx',
-      type: 'Daemon',
-      health: 90,
-      level: 5,
-      attack: 50,
-      defence: 10,
-    };
+      {
+        name: 'Логоваз',
+        type: 'Bowman',
+        health: 50,
+        level: 1,
+        attack: 40,
+        defence: 10,
+      },
+      {
+        name: 'Гендальф',
+        type: 'Magician',
+        health: 60,
+        level: 3,
+        attack: 40,
+        defence: 10,
+      },
+      {
+        name: 'xxxTrueNagibXxx',
+        type: 'Daemon',
+        health: 90,
+        level: 5,
+        attack: 50,
+        defence: 10,
+      },
+    ];
   }
 
   *[Symbol.iterator]() {
-    for (let value = 0; value < Object.values(this).length; value++) {
-      yield Object.values(this)[value];
+    const { teams } = this;
+    for (let value = 0; value < teams.length; value++) {
+      yield teams[value];
     }
   }
 }
